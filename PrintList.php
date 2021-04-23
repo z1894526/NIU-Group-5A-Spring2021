78fca03dc0cdf1805fe1da515139c76c36d65ab3
z1894526 <70327072+z1894526@users.noreply.github.com>
2021-04-23 12:13:33 -0500

    Update PrintList.php

--- PrintList.php
+++ PrintList.php
@@ -10,10 +10,7 @@ $Legacypdo;
 function draw_table_order($rows) {
     echo "<head>
     <style>
-        body {background-color: white;}
         table {text-align: center;}
-        tr {padding-top: 10px;}
-        img { object-fit: contain}
     </style>
     </head>";
     echo "<table border=1 cellspacing=1 cellpadding=1>";
