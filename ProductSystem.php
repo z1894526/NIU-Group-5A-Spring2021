963f34b0203e2de954af11959ec543634769999a
z1894526 <70327072+z1894526@users.noreply.github.com>
2021-04-23 12:35:59 -0500

    Update ProductSystem.php

--- ProductSystem.php
+++ ProductSystem.php
@@ -5,22 +5,22 @@
 <?php include 'style.css'; ?>
 </style>
 <body>
-<div class="container">
-    <form class="smallerButton" method="POST" action="CustomerInterface.php">
-        <input type="submit" class="buttonStyle" value="Customer"/>
-    </form>
-    </br>
-    <form class="smallerButton"method="POST" action="ReceivingDeskInterface.php">
-        <input type="submit" class="buttonStyle" value="Receiving Desk"/>
-    </form>
-    </br>
-    <form class="smallerButton" method="POST" action="WarehouseInterface.php">
-        <input type="submit" class="buttonStyle" value="Warehouse"/>
-    </form>
-    </br>
-    <form class="smallerButton" method="POST" action="Admin.php">
-        <input type="submit" class="buttonStyle" value="Admin Console"/>
-    </form>
-</div>
+    <div class="centerContainer" style="padding: 20px;">
+        <form class="smallerButton" method="POST" action="CustomerInterface.php">
+            <input type="submit" class="buttonStyle" value="Customer"/>
+        </form>
+        </br>
+        <form class="smallerButton"method="POST" action="ReceivingDesk.php">
+            <input type="submit" class="buttonStyle" value="Receiving Desk"/>
+        </form>
+        </br>
+        <form class="smallerButton" method="POST" action="WarehouseInterface.php">
+            <input type="submit" class="buttonStyle" value="Warehouse"/>
+        </form>
+        </br>
+        <form class="smallerButton" method="POST" action="AdminConsoleInterface.php">
+            <input type="submit" class="buttonStyle" value="Admin Console"/>
+        </form>
+    </div>
 </body>
 </html>
