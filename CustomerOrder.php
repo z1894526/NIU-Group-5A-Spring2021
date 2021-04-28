2bbec3d3376566770bb73aa86c4dd8a6609e09e8
MegaHaxor <z1880484@hopper.cs.niu.edu>
2021-04-28 13:43:04 -0500

    Added not-pretty field checking for new customer

--- CustomerOrder.php
+++ CustomerOrder.php
@@ -111,19 +111,78 @@ if (isset($_SESSION['cart'])) {
       <h2>New Customer Form</h2> 
       <form style="text-align: left;" method="post"> 
          <label>First Name</label><br/> 
-         <input type="text" class="inputTextSelect" placeholder="first name..." name="first_name"/><br/><br/> 
+		 <input type="text" class="inputTextSelect" placeholder="first name..." name="first_name" required/>
+         <br/><br/> 
          <label>Last Name</label><br/> 
-         <input type="text" class="inputTextSelect" placeholder="last name..." name="last_name"/><br/><br/> 
+		 <input type="text" class="inputTextSelect" placeholder="last name..." name="last_name" required/>
+         <br/><br/> 
          <label>Email</label><br/> 
-         <input type="text" class="inputTextSelect" placeholder="email..." name="email"/><br/><br/> 
+		 <input type="text" class="inputTextSelect" placeholder="email..." name="email"
+             pattern="[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+"
+             title = "Must be a valid email address" required/><br/><br/> 
          <label>Street Address</label><br/> 
-         <input type="text" class="inputTextSelect" placeholder="street address..." name="street_addr"/><br/><br/> 
+		 <input type="text" class="inputTextSelect" placeholder="street address..." 
+             name="street_addr" required/><br/><br/> 
          <label>City</label><br/> 
-         <input type="text" class="inputTextSelect" placeholder="city..." name="city_addr"/><br/><br/> 
-         <label>State</label><br/> 
-         <input type="text" class="inputTextSelect" placeholder="state..." name="state_addr"/><br/><br/> 
+		 <input type="text" class="inputTextSelect" placeholder="city..."
+             name="city_addr" required/><br/><br/> 
+         <label>State</label><br/>
+         <select class="inputTextSelect" name="state_addr">
+            <option value="AL">Alabama</option>
+            <option value="AK">Alaska</option>
+            <option value="AZ">Arizona</option>
+            <option value="AR">Arkansas</option>
+            <option value="CA">California</option>
+            <option value="CO">Colorado</option>
+            <option value="CT">Connecticut</option>
+            <option value="DE">Delaware</option>
+            <option value="DC">District Of Columbia</option>
+            <option value="FL">Florida</option>
+            <option value="GA">Georgia</option>
+            <option value="HI">Hawaii</option>
+            <option value="ID">Idaho</option>
+            <option value="IL">Illinois</option>
+            <option value="IN">Indiana</option>
+            <option value="IA">Iowa</option>
+            <option value="KS">Kansas</option>
+            <option value="KY">Kentucky</option>
+            <option value="LA">Louisiana</option>
+            <option value="ME">Maine</option>
+            <option value="MD">Maryland</option>
+            <option value="MA">Massachusetts</option>
+            <option value="MI">Michigan</option>
+            <option value="MN">Minnesota</option>
+            <option value="MS">Mississippi</option>
+            <option value="MO">Missouri</option>
+            <option value="MT">Montana</option>
+            <option value="NE">Nebraska</option>
+            <option value="NV">Nevada</option>
+            <option value="NH">New Hampshire</option>
+            <option value="NJ">New Jersey</option>
+            <option value="NM">New Mexico</option>
+            <option value="NY">New York</option>
+            <option value="NC">North Carolina</option>
+            <option value="ND">North Dakota</option>
+            <option value="OH">Ohio</option>
+            <option value="OK">Oklahoma</option>
+            <option value="OR">Oregon</option>
+            <option value="PA">Pennsylvania</option>
+            <option value="RI">Rhode Island</option>
+            <option value="SC">South Carolina</option>
+            <option value="SD">South Dakota</option>
+            <option value="TN">Tennessee</option>
+            <option value="TX">Texas</option>
+            <option value="UT">Utah</option>
+            <option value="VT">Vermont</option>
+            <option value="VA">Virginia</option>
+            <option value="WA">Washington</option>
+            <option value="WV">West Virginia</option>
+            <option value="WI">Wisconsin</option>
+            <option value="WY">Wyoming</option>
+		 </select>
          <label>Zip</label><br/> 
-         <input type="text" class="inputTextSelect" placeholder="zip..." name="zip_addr"/><br/> 
+		 <input type="text" class="inputTextSelect" placeholder="zip..." name="zip_addr"
+			 pattern="\d{5}(?:[-\s]\d{4})?" title="Must be a valid zip code" required/><br/> 
          <br/> 
          <input type="submit" class="inputSubmit" name="addCustomer" value="Add Customer"> 
       </form>
